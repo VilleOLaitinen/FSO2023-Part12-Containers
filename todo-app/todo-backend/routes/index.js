b644eed6e002ee9d7ebf0b6c3ef8e6e5dcfc876b
Ville Laitinen <ville.o.laitinen@edu.turkuamk.fi>
2023-06-04 01:26:11 +0300

    added --legacy-watch flag to nodemon for hot reload to work on windows

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,27 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const redis = require('../redis')
+const redis = require('../redis');
 
-const configs = require('../util/config')
+const configs = require('../util/config');
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
 router.get('/', async (req, res) => {
-  visits++
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
   });
 });
 
-
 router.get('/statistics', async (req, res) => {
-  let added = await redis.getAsync('added')
-  added = added ? added : 0
+  let added = await redis.getAsync('added');
+  added = added ? added : 0;
 
-  res.json({added_todos: Number(added)})
-})
+  res.json({ added_todos: Number(added) });
+});
 
 module.exports = router;
